87073c015b589f8519269230c2bbfc0a3c7919ab
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-10-23 09:13:15 -0600

    Complete Iteration 1

---
+++ test/patron_test.rb
@@ -0,0 +1,29 @@
+require'./lib/patron'
+require 'minitest/pride'
+require 'minitest/autorun'
+
+class PatronTest < Minitest::Test
+  def setup
+    @bob = Patron.new("Bob", 20)
+  end
+
+  def test_it_exists
+    assert_instance_of Patron, @bob
+  end
+
+  def test_it_has_attributes
+    assert_equal "Bob", @bob.name
+    assert_equal 20, @bob.spending_money
+  end
+
+  def test_it_doesnt_start_with_interests
+    assert_equal [], @bob.interests
+  end
+
+  def test_it_can_gain_interests
+    @bob.add_interest("Dead Sea Scrolls")
+    assert_equal ["Dead Sea Scrolls"], @bob.interests
+    @bob.add_interest("Gems and Minerals")
+    assert_equal ["Dead Sea Scrolls", "Gems and Minerals"], @bob.interests
+  end
+end
