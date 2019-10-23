3d5f48c6ce1c8c8be543048424ca4d5d0ee9fb31
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-10-23 09:33:00 -0600

    Complete Iteration 2

---
+++ lib/museum.rb
@@ -0,0 +1,18 @@
+class Museum
+  attr_reader :name, :exhibits
+
+  def initialize(name)
+    @name = name
+    @exhibits = []
+  end
+
+  def add_exhibit(exhibit)
+    @exhibits << exhibit
+  end
+
+  def recommend_exhibits(patron)
+    exhibits.flat_map do |exhibit|
+      exhibit if patron.interests.include?(exhibit.name)
+    end.compact
+  end
+end
---
+++ test/museum_test.rb
@@ -0,0 +1,52 @@
+require'./lib/exhibit'
+require'./lib/patron'
+require'./lib/museum'
+require 'minitest/pride'
+require 'minitest/autorun'
+
+class MuseumTest < Minitest::Test
+  def setup
+    @dmns = Museum.new("Denver Museum of Nature and Science")
+
+    @gems_and_minerals = Exhibit.new("Gems and Minerals", 0)
+    @dead_sea_scrolls = Exhibit.new("Dead Sea Scrolls", 10)
+    @imax = Exhibit.new("IMAX", 15)
+
+    @bob = Patron.new("Bob", 20)
+    @sally = Patron.new("Sally", 20)
+  end
+
+  def test_it_exists
+    assert_instance_of Museum, @dmns
+  end
+
+  def test_it_has_a_name
+    assert_equal "Denver Museum of Nature and Science", @dmns.name
+  end
+
+  def test_it_starts_without_exhibits
+    assert_equal [], @dmns.exhibits
+  end
+
+  def test_it_can_gain_exhibits
+    @dmns.add_exhibit(@gems_and_minerals)
+    assert_equal [@gems_and_minerals], @dmns.exhibits
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    assert_equal [@gems_and_minerals, @dead_sea_scrolls], @dmns.exhibits
+    @dmns.add_exhibit(@imax)
+    assert_equal [@gems_and_minerals, @dead_sea_scrolls, @imax], @dmns.exhibits
+  end
+
+  def test_recommend_exhibits
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+    
+    @bob.add_interest("Dead Sea Scrolls")
+    @bob.add_interest("Gems and Minerals")
+    @sally.add_interest("IMAX")
+
+    assert_equal [@gems_and_minerals, @dead_sea_scrolls], @dmns.recommend_exhibits(@bob)
+    assert_equal [@imax], @dmns.recommend_exhibits(@sally)
+  end
+end
